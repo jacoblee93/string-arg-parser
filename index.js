c46c85296fe3a8769694fe7e82e5677515ead023
Jacob <jacoblee93@gmail.com>
2019-08-08 12:28:22 -0700

    Adds quote support

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = {
   parse: (inputString) => {
-    let params = inputString.split(/\s/);
+    let params = inputString.match(/(("|').*?("|')|[^("|')\s]+)+(?=\s*|\s*$)/g);
     return params.reduce((parsedParams, param) => {
       let kvPair = param.split('=');
       if (kvPair.length !== 2) {
         return parsedParams;
       }
-      parsedParams[kvPair[0]] = kvPair[1];
+      parsedParams[kvPair[0]] = kvPair[1].replace(/(^")|("$)/g, '');
       return parsedParams;
     }, {})
   }
